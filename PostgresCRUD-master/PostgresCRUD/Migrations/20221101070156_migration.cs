9c669443955ff303b33e625c3090d5acee03f693
ferhat <cicekf256@gmail.com>
2022-11-04 21:04:51 +0300

    ...

---
+++ PostgresCRUD-master/PostgresCRUD/Migrations/20221101070156_migration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PostgresCRUD.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    surname = table.Column<string>(type: "text", nullable: true),
+                    gender = table.Column<char>(type: "character(1)", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: true),
+                    date_of_birth = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    number_phone = table.Column<string>(type: "text", nullable: true),
+                    gmail = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
